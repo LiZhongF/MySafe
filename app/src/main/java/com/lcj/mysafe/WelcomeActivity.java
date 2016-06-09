93988075e2de79b2efd133cdc2ca92f072825cdf
jie <1215912978@qq.com>
2016-06-09 23:34:04 +0800

    0.1

--- app/src/main/java/com/lcj/mysafe/WelcomeActivity.java
+++ app/src/main/java/com/lcj/mysafe/WelcomeActivity.java
@@ -2,9 +2,10 @@ package com.lcj.mysafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class WelcomeActivity extends Activity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
